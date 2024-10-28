f74fd492ca6da1c9f0507da7627c73cd4535c7fd
Mauro Marzocca <57107500+mauromarzocca@users.noreply.github.com>
2024-10-28 12:44:17 +0100

    Rebuild Repo

---
+++ Old Build/7/7.4.2/config.py
@@ -0,0 +1,24 @@
+import os
+from datetime import datetime
+
+# Configurazione del logging
+anno_corrente = datetime.now().strftime('%Y')
+mese_corrente = datetime.now().strftime('%m')
+data_corrente = datetime.now().strftime("%Y-%m-%d")
+
+cartella_log = os.path.join('log', anno_corrente, mese_corrente)
+nome_file = f"{cartella_log}/{data_corrente}.txt"
+
+# Configurazione del bot
+bot_token = 'IL_TUO_BOT_TOKEN'
+chat_id = 'IL_TUO_CHAT_ID'
+autorizzati = [12345678]  # Lista di ID Telegram autorizzati
+indirizzi_ping = [
+    {'nome': 'Dispositivo1', 'indirizzo': '192.168.1.1'},
+    {'nome': 'Dispositivo2', 'indirizzo': '192.168.1.2'},
+]
+
+# Credenziali MySQL
+DB_USER = 'tuo_utente'
+DB_PASSWORD = 'tua_password'
+DB_NAME = 'NetworkAllarm'
