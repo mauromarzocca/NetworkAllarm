229c9c02cbbb3ac136759a4c83b571b4100f5f0e
Mauro Marzocca <57107500+mauromarzocca@users.noreply.github.com>
2024-11-08 17:16:50 +0100

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,8 @@ nome_file = f"{cartella_log}/{data_corrente}.txt"
 # Configurazione del bot
 bot_token = 'IL_TUO_BOT_TOKEN'
 chat_id = 'IL_TUO_CHAT_ID'
-autorizzati = [12345678]  # Lista di ID Telegram autorizzati
+autorizzati = []  # Lista di ID Telegram autorizzati
+# Esempio autorizzati = [12345678]
 indirizzi_ping = [
     #{'nome': 'Dispositivo1', 'indirizzo': '192.168.1.1'},
     #{'nome': 'Dispositivo2', 'indirizzo': '192.168.1.2'},
